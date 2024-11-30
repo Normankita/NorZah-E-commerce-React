906ba01fddd407442f4b475ae7803f11a628dfbe
Normankita <kitanorman1@gmail.com>
2024-11-30 09:20:16 +0300

    complete- navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router-dom";
 
 export const Header = ({title, subtitle}) => {
   return (
     <div className="header">
+      <NavLink to="/"><span>Home</span></NavLink>
+      <NavLink to="/jokes"><span>Jokes</span></NavLink>
         <h1>{title}</h1>
         <p>{subtitle}</p>
     </div>
--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -4,7 +4,7 @@ import { Joke } from "../components/Joke";
 export const Jokes = () => {
   return (
     <div>
-      <Header/>
+      <Header title={"Jokes"} subtitle={"get random Chuck Noris Jokes here"}/>
       <Joke/> 
     </div>
   )
